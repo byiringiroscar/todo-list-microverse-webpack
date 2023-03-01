d6050603aca0db5af11a2733eea0fb69f4f89113
byiringiroscar <byiringoroscar@gmail.com>
2023-03-01 11:22:36 +0200

    fix linters in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'development',
