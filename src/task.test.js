ffdf49ef42fdabff1918d5d3bf38b8577fbb7217
byiringiroscar <byiringoroscar@gmail.com>
2023-03-09 16:10:48 +0200

    fix linters errors

--- src/task.test.js
+++ src/task.test.js
@@ -1,6 +1,6 @@
-import DoTask from "./task.js";
+import DoTask from './task.js';
 
-require("jest-localstorage-mock"); // eslint-disable-line import/no-unresolved
+require('jest-localstorage-mock'); // eslint-disable-line import/no-unresolved
 
 beforeEach(() => {
   localStorage.clear();
@@ -8,44 +8,44 @@ beforeEach(() => {
   localStorage.setItem.mockClear();
 });
 
-describe("Add and remove Task", () => {
+describe('Add and remove Task', () => {
   const newTask = new DoTask();
-  newTask.addData("google is my dream");
-  it("google is my dream was added to list", () => {
-    expect(newTask.tasks[0].description).toBe("google is my dream");
+  newTask.addData('google is my dream');
+  it('google is my dream was added to list', () => {
+    expect(newTask.tasks[0].description).toBe('google is my dream');
   });
 
-  it("delete data", () => {
+  it('delete data', () => {
     newTask.removeData([0]);
     expect(newTask.tasks.length).toBe(0);
   });
 });
 
-describe("Edit, update status and clear", () => {
+describe('Edit, update status and clear', () => {
   const newTask = new DoTask();
-  newTask.addData("google is my dream");
-  newTask.editLocal(0, "google is my future dream");
-  it("google is my dream was added to list and is going to be edited", () => {
-    expect(newTask.tasks[0].description).toBe("google is my future dream");
+  newTask.addData('google is my dream');
+  newTask.editLocal(0, 'google is my future dream');
+  it('google is my dream was added to list and is going to be edited', () => {
+    expect(newTask.tasks[0].description).toBe('google is my future dream');
   });
-  it("change status complete to true", () => {
+  it('change status complete to true', () => {
     const dataObject = newTask.tasks[0];
     dataObject.completed = true;
     const updateData = JSON.stringify(dataObject);
     newTask.updateComplete(updateData);
     expect(newTask.tasks[0].completed).toBe(true);
   });
-  it("change status complete to false", () => {
+  it('change status complete to false', () => {
     const dataObject = newTask.tasks[0];
     dataObject.completed = false;
     const updateData = JSON.stringify(dataObject);
     newTask.updateComplete(updateData);
     expect(newTask.tasks[0].completed).toBe(false);
   });
-  it("clear all complete data", () => {
-    newTask.addData("day one");
-    newTask.addData("day two");
-    newTask.addData("day three");
+  it('clear all complete data', () => {
+    newTask.addData('day one');
+    newTask.addData('day two');
+    newTask.addData('day three');
     newTask.removeData([0, 1, 2, 3, 4]);
     expect(newTask.tasks.length).toBe(0);
   });
